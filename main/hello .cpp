b8279a075a0a92958b986fb741680f1031169464
PES1UG22CS102 <pes1202202305@pesu.pes.edu>
2025-03-20 20:36:48 +0530

    Update hello .cpp

--- main/hello .cpp
+++ main/hello .cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hi, I am Ankit\n";
+  cout << "Hi, I am Anshuman\n";
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
