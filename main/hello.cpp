0dce9059f7eae43ed299728b3ff9ef32936db849
PES1UG22CS102 <pes1202202305@pesu.pes.edu>
2025-03-20 21:05:02 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hi, I am Anshuman\n";
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "I have successfully built and run this\n";
   cout << "CC lab 8\n";
   return 0;
 }
